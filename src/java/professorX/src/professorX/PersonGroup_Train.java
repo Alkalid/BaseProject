3f62990968788a71cb31f46df382030eefa23d95
Alkalid <kartd80165@gmail.com>
2019-12-03 01:30:50 +0800

    新增 java程式碼: PersonGroup_Train.java
    此 sample code 可將 PersonGroup 定型，才能使用它來執行識別

---
+++ src/java/professorX/src/professorX/PersonGroup_Train.java
@@ -0,0 +1,48 @@
+package professorX;
+
+//// This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
+
+public class PersonGroup_Train 
+{
+ public static void main(String[] args) 
+ {
+     HttpClient httpclient = HttpClients.createDefault();
+
+     try
+     {
+    	 String personGroupId = "test_group";
+         URIBuilder builder = new URIBuilder("https://eastasia.api.cognitive.microsoft.com/face/v1.0/persongroups/"+ personGroupId +"/train");
+
+
+         URI uri = builder.build();
+         HttpPost request = new HttpPost(uri);
+         request.setHeader("Ocp-Apim-Subscription-Key", "30d2ac8ee3bb4df0a42e82b24b315599");
+
+
+         // Request body
+         //StringEntity reqEntity = new StringEntity("{body}");
+         //request.setEntity(reqEntity);
+
+         HttpResponse response = httpclient.execute(request);
+         HttpEntity entity = response.getEntity();
+
+         if (entity != null) 
+         {
+             System.out.println(EntityUtils.toString(entity));
+         }
+     }
+     catch (Exception e)
+     {
+         System.out.println(e.getMessage());
+     }
+ }
+}
