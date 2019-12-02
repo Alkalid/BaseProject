5bc3b0fd6d6faa04dab01b79e80d8926dd77c6f9
Alkalid <kartd80165@gmail.com>
2019-12-03 00:06:33 +0800

    新增 java程式碼: Face_Detect.java
    此sample code 可將圖片url 轉為faceID

--- src/java/professorX/src/professorX/Face_Detect.java
+++ src/java/professorX/src/professorX/Face_Detect.java
@@ -1,18 +1,16 @@
 package professorX;
-
-
 //// This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
 import java.net.URI;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPatch;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.util.EntityUtils;
 
-public class PersonGroupPerson_Update 
+public class Face_Detect 
 {
  public static void main(String[] args) 
  {
@@ -20,29 +18,33 @@ public class PersonGroupPerson_Update
 
      try
      {
-    	 //目前中文還無法設定。
-    	 String personId = "ce5b9b0c-ff40-41c3-8f21-b414b06259f9";
-    	 String UpdateName = "xu";
-    	 String UpdateUserData = "FCU IECS Teacher";
+    	 String url = "https://images.chinatimes.com/newsphoto/2018-03-03/900/20180303001535.jpg";
     	 
-         URIBuilder builder = new URIBuilder("https://eastasia.api.cognitive.microsoft.com/face/v1.0/persongroups/test_group/persons/"+personId);
+         URIBuilder builder = new URIBuilder("https://eastasia.api.cognitive.microsoft.com/face/v1.0/detect");
 
+         builder.setParameter("returnFaceId", "true");
+         builder.setParameter("returnFaceLandmarks", "false");
+         //builder.setParameter("returnFaceAttributes", "{string}");
+         builder.setParameter("recognitionModel", "recognition_01");
+         builder.setParameter("returnRecognitionModel", "false");
+         builder.setParameter("detectionModel", "detection_01");
+         //detection_01:普通       detection_02最新
 
          URI uri = builder.build();
-         HttpPatch request = new HttpPatch(uri);
+         HttpPost request = new HttpPost(uri);
          request.setHeader("Content-Type", "application/json");
          request.setHeader("Ocp-Apim-Subscription-Key", "30d2ac8ee3bb4df0a42e82b24b315599");
 
 
          // Request body
          StringEntity reqEntity = new StringEntity(
+        		 
         		 "{" + 
-	       
-	         		"    \"name\": \"" + UpdateName + "\", " +
-	        
-	         		"    \"userData\": \"" + UpdateUserData +"\", " +
+        			       
+	         		"    \"url\": \"" + url + "\" " +
 	         		
 	         		"}"
+        		 
         		 );
          request.setEntity(reqEntity);
 
