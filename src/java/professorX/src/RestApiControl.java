189f7f12cd57def53a6a659722552bed08ced9f2
Alkalid <kartd80165@gmail.com>
2019-12-04 01:21:48 +0800

    測試  java程式碼: RestApiControl.java
    使用不同的圖片url 來測試能否辨識出正確的人物

--- src/java/professorX/src/RestApiControl.java
+++ src/java/professorX/src/RestApiControl.java
@@ -19,7 +19,9 @@ public class RestApiControl
 	
 	public void face_identify()
 	{
-		String url = "https://images.wealth.com.tw/uploads/article/image/20414/rect_1200_600_%E8%94%A1%E8%8B%B1%E6%96%87_900.jpg";
+		String url = "https://lh3.googleusercontent.com/HWAQ4ZeECoasI3llQQwjKD8ZortJ9DHU3eO9yMDRxR64DkiB_YVnXsGBockTllbcpOBR7QVXgb4ko6yE5ufJfHqrOT3OyLKcvB7tzM956sx8z8CUSFxOKMmmxfkHPFyJaKfpXTGMzg1THDtYrdD67cgspD8aK_sxp3ms6RfsT0ZydNoXeqZT-73KLlKWTN5J5srZ1bRLpZQ4Cwi0SDhst4RU3vkzp6Nk2uTuPDUIkGM3IrKthI5uBSqyP0ZH0_WaS7BloeoqDGPhEZlURhm3XjJkpYouCAIPc_nNYbeDktsY5ZW80QzbLBW1kE59ny6CPnB6gmlYFVoivorm5vAOS85ClQumLutESUHhG6lJkxc1L8g8ESeHY387SLDVCdwARybJ7Ki4y4AgXOZCK32Q02ouEPmO9DMcSdvI-c9tc2_R8_sXvmSkJRMazQ_OSoCjuCQXxc16UCDRZc1N33X7j6itMaPKuGUyNxmBcMZ7E9iYuJI1P9BskUohbCclJTXHrRaFT4suINvR4ndL2bKw-Kgu_5N_gavnt-ePdetUF8UUwlehGrBPlE6OYewpyOzHOJGlxvB472ttlq3xOtgYfcZ47ubnWL3D23i5xN_UbsqNpr5p-Uo2lC9dKMia_5iiX2x0wY_vk5yP8XXJi3QmI51Ee7EQYG2gW0P44SCeEKUimnM_bIlz9Q=w690-h920-no";
+		
+		
 		Face_Detect Detect = new Face_Detect();
 		String faceId = Detect.Face_Detect(url);
 		faceId = faceId.substring(1, faceId.length() -1 );
