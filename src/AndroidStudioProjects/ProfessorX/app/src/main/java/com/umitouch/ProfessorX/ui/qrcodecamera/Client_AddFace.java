411256fa36e94dcdb29ef600efb9473e1fcbdb59
Alkalid <kartd80165@gmail.com>
2019-12-16 22:16:24 +0800

    新增 Android Stdio程式碼 :Client_AddFace
    Client_AddFace 此為"新增人物"功能，所使用的伺服器連線物件

---
+++ src/AndroidStudioProjects/ProfessorX/app/src/main/java/com/umitouch/ProfessorX/ui/qrcodecamera/Client_AddFace.java
@@ -0,0 +1,127 @@
+package com.umitouch.ProfessorX.ui.qrcodecamera;
+
+import android.os.Handler;
+import android.util.Log;
+
+import com.umitouch.ProfessorX.Fragment_AddFace;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class Client_AddFace extends Thread
+{
+    Fragment_AddFace FA;
+    Socket socket = null;
+    BufferedWriter bw = null;
+    BufferedReader br = null;
+
+    String tmp;
+    public String SocketInstruct = "";
+
+    public String LobbyData_String = "";
+
+    private String  RT_String;
+    private Boolean Vision_lock = false;
+
+    public String   L_RT_String   = "";
+    private Boolean L_Vision_lock = false;
+
+
+
+    boolean isTerminated = false;
+
+    int che6 = 6,che7 = 7,che8 = 8,che9 = 9;
+
+    public static Handler mHandler = new Handler();
+
+    public void setSource(Fragment_AddFace FA)
+    {
+        this.FA = FA;
+    }
+
+    public void sentMessage()
+    {
+        try
+        {
+            bw.write(this.SocketInstruct+"\n");
+            bw.flush();
+        }
+        catch (IOException e)
+        {
+
+        }
+    }
+    public boolean isTerminated()
+    {
+        isTerminated = true;
+        return isTerminated;
+    }
+    public void push(String str)
+    {
+        String[] SData = str.split(String.valueOf((char)(che9)))[1].split(String.valueOf((char)(che7)));
+
+
+
+    }
+
+    public void setSocketInstruct(String SocketInstruct)
+    {
+        this.SocketInstruct = SocketInstruct;
+    }
+
+    public void connect()
+    {
+        try
+        {
+            socket = new Socket("114.35.11.36",5251);
+
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            bw = new BufferedWriter( new OutputStreamWriter(socket.getOutputStream()));
+        }
+        catch (IOException e)
+        {
+
+        }
+    }
+
+    @Override
+    public void run()
+    {
+        String content = null;
+        try
+        {
+            if (!isTerminated)
+            {
+                connect();
+                sentMessage();
+            }
+
+            isTerminated = socket.isConnected();
+
+            while (socket.isConnected())
+            {
+                while ((content = br.readLine()) != null)
+                {
+                    tmp = content;
+                    mHandler.post(updateText);
+                }
+            }
+        }
+        catch (IOException e)
+        {
+
+        }
+    }
+
+    private Runnable updateText = new Runnable() {
+        public void run() {
+            // 加入新訊息並換行
+            push(tmp);
+        }
+    };
+
+}
