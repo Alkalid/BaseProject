227e3fa46e09675ba486de67eaafa8888f3a7ef7
Alkaloid <kartd80165@gmail.com>
2019-12-16 11:32:08 +0800

    修改 java程式碼: RestApiControl.java
    設定函式:face_identify
    回傳值為一個linkedList 
    1. 是否辨識成功 fail / success
    2. personID
    3. 信心指數

--- src/java/professorX/src/RestApiControl.java
+++ src/java/professorX/src/RestApiControl.java
@@ -1,7 +1,7 @@
 import professorX.Face_Identify;
 import org.json.*;
 import org.apache.http.util.EntityUtils;
-
+import java.util.LinkedList;
 
 import professorX.Face_Detect;
 
@@ -14,12 +14,16 @@ public class RestApiControl
 	{
 		RestApiControl rc = new RestApiControl();
 		
-		rc.face_identify();
+		//rc.face_identify();
 	}
 	
-	public void face_identify()
+	public LinkedList face_identify(String url )
 	{
-		String url = "http://114.35.11.36/IMG_20190701_101113.jpg"; //
+		//String url = "http://114.35.11.36/IMG_20190701_101113.jpg"; //
+		
+		//String url =  "https://upload.wikimedia.org/wikipedia/commons/1/1b/%E8%94%A1%E8%8B%B1%E6%96%87%E5%AE%98%E6%96%B9%E5%85%83%E9%A6%96%E8%82%96%E5%83%8F%E7%85%A7.png";
+		//String url = "https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/07/12/realtime/6556698.jpg&x=0&y=0&sw=0&sh=0&exp=3600";
+		LinkedList Identify_result = new LinkedList();
 		
 		
 		Face_Detect Detect = new Face_Detect();
@@ -42,14 +46,29 @@ public class RestApiControl
 		//System.out.println("personId : " + personId );
 		
 		//JSONObject IdentifyResult = new JSONObject(personId);
-/////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////
 		JSONObject j =  new JSONObject(personId);
-		Object jsonOb = j.getJSONArray("candidates").getJSONObject(0).getString("personId");
 		
-		System.out.println( jsonOb );		//這是回傳結果perconid
+		if(j.getJSONArray("candidates").length() == 0 )//if 
+		{
+			System.out.println( "fail"  );
+			Identify_result.add("fail" );
+		}
+		else
+		{
+			System.out.println( "success"  );
+			
+			
+			Object jsonOb = j.getJSONArray("candidates").getJSONObject(0).getString("personId");
+			Object jsonOb2 = j.getJSONArray("candidates").getJSONObject(0).get("confidence");
+			
+			Identify_result.add("success" );
+			Identify_result.add(jsonOb.toString() );
+			Identify_result.add(jsonOb2.toString() );
+			System.out.println( "\n"+jsonOb+"\n"+jsonOb2  );		//這是回傳結果perconid
+		}
 		
-		
-		
+		return Identify_result;
 	}
 
 }
