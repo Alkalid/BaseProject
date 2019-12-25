edba66874063e4ff141515c4ac0603b941bd15bc
Wilson Chen <d0745513@mail.fcu.edu.tw>
2019-12-25 18:30:41 +0800

    嘿嘿我新增了我的頁面囉

--- src/AndroidStudioProjects/ProfessorX/app/src/main/java/com/umitouch/ProfessorX/Fragment_AddFace.java
+++ src/AndroidStudioProjects/ProfessorX/app/src/main/java/com/umitouch/ProfessorX/Fragment_AddFace.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Fragment_AddFace extends Fragment
 {
@@ -14,6 +15,12 @@ public class Fragment_AddFace extends Fragment
     private MainActivity MA;
     private Button UMi_Button;
     private  int che9 = 9;
+
+    private EditText id_EditText;
+    private EditText name_EditText;
+    private EditText fb_EditText;
+    private EditText ig_EditText;
+    private EditText identity_EditText;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
@@ -26,8 +33,11 @@ public class Fragment_AddFace extends Fragment
         }
     });
         // Inflate the layout for this fragment
-
-
+        id_EditText = (EditText)view.findViewById(R.id.id);
+        name_EditText=(EditText)view.findViewById(R.id.name);
+        fb_EditText=(EditText)view.findViewById(R.id.fb);
+        ig_EditText=(EditText)view.findViewById(R.id.ig);
+        identity_EditText=(EditText)view.findViewById(R.id.identity);
        // CreatePerson("[img]https://upload.cc/i1/2019/12/18/naEAdh.jpg","王子嘉","https://www.facebook.com/profile.php?id=100003612286269","https://www.instagram.com/zi7909/?hl=zh-tw","學校:逢甲大學資工系","uml1uYPeVTUJU");
         CreatePerson("https://upload.cc/i1/2019/12/18/4b8V6z.jpg","林翊庭","https://www.facebook.com/lintippilin","https://www.instagram.com/tippi1105/?hl=zh-tw","學校:逢甲大學行銷系","uml1uYPeVTUJU");
 
@@ -42,7 +52,12 @@ public class Fragment_AddFace extends Fragment
 
     public void AddSuccess()
     {
-
+        id_EditText.getText().toString();
+        name_EditText.getText().toString();
+        fb_EditText.getText().toString();
+        ig_EditText.getText().toString();
+        identity_EditText.getText().toString();
+        CreatePerson();
     }
 
     public void CreatePerson(String URL,String person_name,String person_fb,String person_ig ,String person_info ,String uid  )
